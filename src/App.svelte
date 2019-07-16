84af08c6e473853a21c5e0cb1c27911dad3b44bb
Andrew Davis <atd285@gmail.com>
2019-07-15 22:31:40 -0400

    add sounds to landing page!

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import sounds from "./sounds.json";
-  console.log(sounds);
+
   const sections = Object.keys(sounds).map(section => ({
     title: section[0].toLocaleUpperCase() + section.slice(1),
     section
@@ -8,25 +8,40 @@
 
   console.log(sections);
   console.log(sounds["hits"]);
+
+  function playSound(section, file) {
+    const path = `sounds/${section}/${file}`;
+    console.log("playing", { path, file });
+  }
 </script>
 
 <style>
-  h1 {
-    color: purple;
-  }
   .sound-button {
-    display: inline-block;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid red;
+    border-radius: 4px;
+    padding: 0.5em;
+    height: 130px;
+    width: 130px;
+    cursor: pointer;
+    margin: 0.5em;
   }
   .container {
     height: 100%;
-    display: flex;
     width: 100%;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  .title {
     text-align: center;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+  .sound-button:hover {
+    background: yellow;
+    color: #000;
   }
 </style>
 
@@ -34,11 +49,13 @@
   {#each sections as { title, section }}
     <div>
       <h2 class="title">{title}</h2>
-      {#each sounds[section] as { title, name }}
-        <div class="sound-button">
-          <h4>{name}</h4>
-        </div>
-      {/each}
+      <div class="sound-section">
+        {#each sounds[section] as { file, name }}
+          <div class="sound-button" on:click={() => playSound(section, file)}>
+            <h4>{name}</h4>
+          </div>
+        {/each}
+      </div>
     </div>
   {/each}
 </div>
