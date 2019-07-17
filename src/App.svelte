dd090d0a4189d2e45bb5246f6a38fab5bbdf1895
Andrew Davis <atd285@gmail.com>
2019-07-16 21:39:11 -0400

    add responsive styling to homepage

--- src/App.svelte
+++ src/App.svelte
@@ -8,54 +8,63 @@
 
   console.log(sections);
   console.log(sounds["hits"]);
-
+  const audio = new Audio();
   function playSound(section, file) {
     const path = `sounds/${section}/${file}`;
-    console.log("playing", { path, file });
+    audio.src = path;
+    audio.play();
   }
 </script>
 
 <style>
   .sound-button {
+    position: relative;
     display: inline-flex;
     justify-content: center;
+    flex-direction: column;
     align-items: center;
-    border: 1px solid red;
-    border-radius: 4px;
-    padding: 0.5em;
-    height: 130px;
-    width: 130px;
+    outline: 1px solid #ff4141;
+    padding: 1.5em;
+    height: 10vw;
+    width: 10vw;
     cursor: pointer;
-    margin: 0.5em;
+    margin: 0.25em;
+    font-size: 2vw;
+    color: #fff200;
+    box-shadow: 3px 3px 3px #ff4141;
+    transition: background 0.25s linear, color 0.25s linear,
+      box-shadow 0.15s linear;
   }
   .container {
     height: 100%;
     width: 100%;
     text-align: center;
-    cursor: pointer;
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    flex-wrap: wrap;
-    flex-direction: column;
   }
   .sound-button:hover {
-    background: yellow;
+    background: #fff200;
     color: #000;
+  }
+  .sound-button:active {
+    transition: translate(5px, 5px);
+    box-shadow: none;
+  }
+  .section-title {
+    position: absolute;
+    bottom: 1vw;
   }
 </style>
 
 <div class="container">
   {#each sections as { title, section }}
-    <div>
-      <h2 class="title">{title}</h2>
-      <div class="sound-section">
-        {#each sounds[section] as { file, name }}
-          <div class="sound-button" on:click={() => playSound(section, file)}>
-            <h4>{name}</h4>
-          </div>
-        {/each}
+    {#each sounds[section] as { file, name }}
+      <div class="sound-button" on:mousedown={() => playSound(section, file)}>
+        <h4>{name}</h4>
+        <small class="section-title">{section}</small>
       </div>
-    </div>
+    {/each}
   {/each}
 </div>
